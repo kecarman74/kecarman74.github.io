44e762a762fa560bc1a5bf75f2003f19f5af984d
Unknown <kecarman74@gmail.com>
2017-10-19 20:57:12 -0700

    Created p5.js

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,19 @@
+var j, k, x, y = global
+var j = 0;
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  if(mouseIsPressed) {
+  		fill(0, j , 0, 100);
+  		var j = j + 1;
+	} 
+	else {
+		fill(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), 160);
+		var j = 0;
+	}
+	ellipse(mouseX, mouseY, 80, 80);
+	
+}
